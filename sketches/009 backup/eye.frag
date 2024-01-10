b19cd811a88f49ae3da63f14f32d8c202532a258
Zikian <ilianbronchart@protonmail.com>
2024-01-10 02:02:57 +0100

    update eye shader

--- sketches/009 backup/eye.frag
+++ sketches/009 backup/eye.frag
@@ -4,17 +4,17 @@ precision highp float;
 
 varying vec2 vTexCoord;
 
-#define MAX_STARS 700
-#define MAX_ITERATIONS 300
+#define MAX_STARS 1000
 
 uniform float time;
 uniform vec2 resolution;
-uniform vec2 stars[MAX_STARS];
-uniform float eyeOpen;
-uniform float deltaEyeOpen;
-uniform float circleSize;
-uniform float iterationScales[MAX_ITERATIONS];
-uniform int numIterations;
+uniform sampler2D prevTexture;
+uniform vec3 stars[MAX_STARS];
+uniform float paramA;
+uniform float deltaParamA;
+uniform float paramB;
+uniform float paramC;
+uniform float paramD;
 
 const float PI = 3.1415926535897932384626433832795;
 const vec4 bgColor = vec4(0.0, 0.0, 0.0, 1.0);
@@ -26,28 +26,28 @@ float gaussian(float x, float xScale) {
 }
 
 float eyeOpenness() {
-    return pow(eyeOpen, 5.0);
+    return pow(paramA, 5.0);
 }
 
 float f(float x, float hScale) {
     float mult =  PI / 2.0;
-    float bound = PI / (mult * eyeOpen);
+    float bound = PI / (mult * paramA);
 
     if (-bound <= x && x <= bound) {
-        return eyeOpenness() * (cos(eyeOpen * mult * x) * 0.5 + 0.5) * hScale;
+        return eyeOpenness() * (cos(paramA * mult * x) * 0.5 + 0.5) * hScale;
     }
 
     return 0.0;
 }
 
 float g(float x) {
-    return exp(-pow(circleSize * x, 2.0));
+    return exp(-pow(paramB * x, 2.0));
 }
 
 float wave(vec2 p) {
-    float d = length(p) - circleSize;
-    float period = 20.0 * pow(eyeOpen, 2.0);
-    float COS = cos(period * d - time + deltaEyeOpen) * 0.5 + 0.5;
+    float d = length(p) - paramB;
+    float period = 20.0 * pow(paramA, 2.0);
+    float COS = cos(period * d - time + deltaParamA) * 0.5 + 0.5;
     float hVal = (COS + g(d)) * g(d) * 0.5;
     return hVal;
 }
@@ -58,8 +58,11 @@ vec4 drawStars(vec2 p, float waveVal) {
 
     float starVal = 0.0;
     for(int i = 0; i < MAX_STARS; i++) {
-        if (stars[i].x == 0.0) break;
-        float d = length(p - stars[i] + 0.5);
+        vec3 star = stars[i];
+        star.xy -= 0.5;
+        if(star.z == 0.0) break;
+
+        float d = length(p - star.xy);
         starVal = max(starVal, (0.001) / d);
     }
 
@@ -74,24 +77,46 @@ vec4 getEyeColor(float waveVal, float eyelidDist) {
     return finalColor;
 }
 
-vec4 halfEye(vec2 p, float waveVal, vec4 prevColor) {    
+vec4 getPrevColor(float finalScale) {
+    float fbScale = paramB / finalScale;
+
+    vec2 scaledSize = vec2(fbScale);
+    vec2 offsetCoords = vTexCoord - 0.5;
+    offsetCoords.x *= -1.0;
+
+    float maskX = step(-scaledSize.x, offsetCoords.x) * step(offsetCoords.x, scaledSize.x);
+    float maskY = step(-scaledSize.y, offsetCoords.y) * step(offsetCoords.y, scaledSize.y);
+
+    if(maskX * maskY > 0.0) {
+        vec2 adjustedCoords = (offsetCoords + scaledSize) / fbScale / 2.0;
+        return texture2D(prevTexture, adjustedCoords);
+    }
+
+    return vec4(0.0, 0.0, 0.0, 0.0);
+}
+
+vec4 halfEye(vec2 p, float finalScale, float waveVal) {
+    if (p.y <= 0.0) return vec4(0.0);
+    
     float fVal = f(p.x, 0.93);
-    float waveEffect = waveVal * 0.15 * eyeOpenness();
+    float waveEffect = waveVal * 0.2 * paramA;
     float innerEdge = fVal + waveEffect;
 
     if(p.y <= innerEdge) {
-        float eyelidDist = smoothstep(0.05, 0.0, innerEdge - p.y);
+        float eyelidDist = smoothstep(0.01, 0.0, innerEdge - p.y);
 
-        float d = length(p) - circleSize;
+        float d = length(p) - paramB;
         if(d < 0.0) {
             // Inside the circle
             waveVal = smoothstep(-0.01, 0.0, d) * waveVal;
             vec4 eyeColor = getEyeColor(waveVal, eyelidDist);
+            vec4 prevColor = getPrevColor(finalScale);
             return mix(prevColor, eyeColor, max(waveVal,eyelidDist));
         }
         
         return getEyeColor(waveVal, eyelidDist);
     }
+
 
     if(p.y > innerEdge || p.y < 0.0) {
         // Eyelid
@@ -104,22 +129,18 @@ vec4 halfEye(vec2 p, float waveVal, vec4 prevColor) {
     return vec4(0.0);
 }
 
-vec4 eye(vec2 p, float scale, vec4 prevColor) {
+vec4 eye(vec2 p, float scale) {
     float finalScale = 3.0 / scale;
     vec2 pScaled = p * finalScale;
     float waveVal = wave(pScaled);
-
+    
     vec4 starColor = drawStars(p, waveVal);
-    vec4 eyeColor = halfEye(vec2(pScaled.x, abs(pScaled.y)), waveVal, prevColor);
+    
+    vec4 upperHalf = halfEye(pScaled, finalScale ,waveVal);
+    vec4 lowerHalf = halfEye(vec2(pScaled.x, -pScaled.y), finalScale, waveVal);
 
+    vec4 eyeColor = upperHalf + lowerHalf;
     return mix(starColor, eyeColor, eyeColor.a);
-}
-
-vec2 rotate(vec2 p, float angle) {
-    float s = sin(angle);
-    float c = cos(angle);
-    mat2 m = mat2(c, -s, s, c);
-    return m * p;
 }
 
 void main() {
@@ -127,19 +148,9 @@ void main() {
     float aspect = resolution.x / resolution.y;
     uv.x *= aspect;
 
-    vec4 prevColor = vec4(0.0, 0.0, 0.0, 1.0);
-    for(int i = 0; i < MAX_ITERATIONS; i++) {
-        float scale = iterationScales[i];
-        if(scale < 0.0001) break;
-
-        // Calculate rotation based on scale
-        // float rotationAngle = scale * PI; // Example linear mapping
-
-        // uv = rotate(uv, rotationAngle);
-
-        vec4 eyeColor = eye(uv, scale, prevColor);
-        prevColor = mix(prevColor, eyeColor, 1.0);
+    for(int i = 0; i < 10000; i++) {
+        vec4 testEye = eye(uv, 1.0);
     }
 
-    gl_FragColor = prevColor;
+    gl_FragColor = eye(uv, 0.6 * paramD);
 }
--- sketches/009/009.sketch.js
+++ sketches/009/009.sketch.js
@@ -2,23 +2,30 @@ import { createGraphics } from "../../custom/utils.js";
 import { Midi, Note } from "../../custom/midi.js";
 
 const s = (p) => {
-  let layer, prev, next, theShader;
-  let fps = 30;
+  let layer, starTexture, theShader;
+  let fps = 60;
   let width = 1000;
   let height = 1000;
 
-  let paramA = 0;
-  let deltaParamA = 0;
-  let paramB = 1;
+  let baseScale = 0.6;
+  let scaleFactor = 0.5;
+  let eyeOpen = 0;
+  let deltaEyeOpen = 0;
+  let circleSize = 1;
   let nearStars = [];
   let midStars = [];
   let farStars = [];
+  let zoomSpeed = 0;
+  let time = 0;
+  let lastTime = 0
+  let deltaTime = 0;
+  let numIterations = 0;
 
   let midi = new Midi(p, [
-    new Note(0),
-    new Note(1),
-    new Note(12, { lerpFactor: 0.2 }),
-    new Note(20, { lerpFactor: 0.05, isSymmetric: true}),
+    new Note(0, { isSymmetric: true, lerpFactor: 0.05 }),
+    new Note(1, { lerpFactor: 0.05 }),
+    new Note(12, { lerpFactor: 0.05 }),
+    new Note(20, { lerpFactor: 0.05 }),
     new Note(14),
     new Note(15),
     new Note(16),
@@ -29,15 +36,14 @@ const s = (p) => {
   ]);
 
   class Star {
-    constructor(depth) {
-      this.x = p.random(0, 2);
-      this.y = p.random(0, 2);
-      this.depth = depth;
-      this.speed = p.random(0.01, 0.015);
+    constructor() {
+      this.x = p.random(0, 1.5);
+      this.y = p.random(0, 1.5);
+      this.speed = p.random(0.5, 0.5);
     }
 
     toArr() {
-      return [this.x, this.y, this.depth];
+      return [this.x, this.y];
     }
   }
 
@@ -51,36 +57,49 @@ const s = (p) => {
     p.rectMode(p.CENTER);
 
     layer = createGraphics(p, width, height, p.WEBGL);
-    prev = createGraphics(p, width, height);
-    next = createGraphics(p, width, height);
+    starTexture = p.createGraphics(32, 32); 
 
     createStarfield();
 
     p.noStroke();
-    p.frameRate(fps);
+    p.frameRate(100);
   };
 
   function updateParams() {
-    paramB = p.map(midi.get(20), -1.0, 1.0, - 0.86, 4.0);
+    let currentTime = p.millis();
+    deltaTime = (currentTime - lastTime) / 1000.0; // Convert milliseconds to seconds
+    lastTime = currentTime;
+    time += deltaTime;
+
+    deltaEyeOpen = eyeOpen - midi.get(12);
+    eyeOpen = midi.get(12);
+
+    circleSize = p.map(midi.get(20), 0.0, 1.0, -0.86, 1.3);
+    
+    zoomSpeed = p.map(midi.get(0), -1.0, 1.0, -0.1, 0.1)
+    zoomSpeed = Math.abs(midi.get(0)) < 0.001 ? 0 : zoomSpeed;
+ 
+    baseScale = Math.max(0.6, baseScale * (1 + zoomSpeed * deltaTime * 50));
+    scaleFactor = 0.3 + midi.get(1) * 0.5;
   }
 
-  function createStarsForLayer(count, depth) {
+  function createStarsForLayer(count) {
     let stars = [];
     for (let i = 0; i < count; i++) {
-      stars.push(new Star(depth));
+      stars.push(new Star());
     }
     return stars;
   }
 
   function createStarfield() {
-    nearStars = createStarsForLayer(100, 1);
-    midStars = createStarsForLayer(200, 2);
-    farStars = createStarsForLayer(300, 3);
+    nearStars = createStarsForLayer(100);
+    midStars = createStarsForLayer(200);
+    farStars = createStarsForLayer(300);
   }
 
   function updateStarsForLayer(stars, speedMultiplier) {
     return stars.map(star => {
-      star.x = (star.x + star.speed * speedMultiplier) % 1;
+      star.x = (star.x + star.speed * speedMultiplier * deltaTime) % 1.5;
       return star;
     });
   }
@@ -91,42 +110,47 @@ const s = (p) => {
     farStars = updateStarsForLayer(farStars, 1 / 4);
   }
 
+  function calculateIterationScales() {
+    // Calculate minimum iterations so that the scale is smaller than 0.01
+    numIterations = Math.ceil(Math.log(0.01 / baseScale) / Math.log(scaleFactor));
+    numIterations = Math.min(numIterations, 1000);
+
+    let iterationScales = Array(300).fill(0);
+    for (let i = 1; i <= numIterations; i++) {
+      let scale = baseScale * Math.pow(scaleFactor, numIterations - i);
+      if (scale < 10) {
+        iterationScales[i-1] = scale;
+      }
+    }
+
+    return iterationScales;
+  }
 
   p.draw = () => {
     midi.updateValues();
-    updateStarfield();
     updateParams();
-
-    [prev, next] = [next, prev];
-
-    let time = (5 * p.frameCount) / fps;
-    deltaParamA = paramA - midi.get(12);
-    paramA = midi.get(12);
-
-    let stars = nearStars
-      .concat(midStars)
-      .concat(farStars)
-      .map((star) => star.toArr());
-
-    next.clear();
-    next.background(0);
+    updateStarfield();
 
     layer.clear();
     layer.shader(theShader);
-    theShader.setUniform("time", time);
+    theShader.setUniform("time",  time * 7);
     theShader.setUniform("resolution", [width, height]);
-    theShader.setUniform("prevTexture", prev);
-    theShader.setUniform("stars", stars.flat());
-    theShader.setUniform("paramA", midi.get(12));
-    theShader.setUniform("deltaParamA", 5 * deltaParamA);
-    theShader.setUniform("paramB", paramB);
-    theShader.setUniform("paramC", midi.get(14));
-    theShader.setUniform("paramD", 1 + p.map(midi.get(15), -1.0, 1.0, -2.0, 2.0));
+    theShader.setUniform(
+      "stars",
+      nearStars
+        .concat(midStars)
+        .concat(farStars)
+        .map((star) => star.toArr())
+        .flat()
+    );
+    theShader.setUniform("eyeOpen", eyeOpen);
+    theShader.setUniform("deltaEyeOpen", 100 * deltaEyeOpen);
+    theShader.setUniform("circleSize", circleSize);
+    theShader.setUniform("iterationScales", calculateIterationScales());
+    theShader.setUniform("numIterations", numIterations);
     layer.rect(0, 0, width, height);
 
-    next.image(layer, 0, 0)
-
-    p.image(next, 0, 0);
+    p.image(layer, 0, 0);
   };
 
   p.keyPressed = () => {
